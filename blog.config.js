7716788b3a8ab922e10584821f48fafeb4c1c7ff
anriclee <anriclee@gmail.com>
2022-06-05 11:03:59 +0800

    change author

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: '我的精神家园',
+  author: 'Anric Lee',
+  email: 'anriclee@gmail.com',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
